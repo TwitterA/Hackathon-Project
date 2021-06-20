92bd0909a0c12bc1fbb85f5b14a8014ac1107fcb
Hanan <82478654+hanan-tuwaiq@users.noreply.github.com>
2021-06-20 14:57:56 +0300

    initial views @NorahmAlmutairi @Albandaryalqaseemi

--- Twitter/Twitter/Data/Migrations/20210620103340_SeedTestTweet.cs
+++ Twitter/Twitter/Data/Migrations/20210620103340_SeedTestTweet.cs
@@ -6,7 +6,7 @@ namespace Twitter.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.InsertData(
+/*            migrationBuilder.InsertData(
                 table: "Tweets",
                 columns: new[] { "Id", "Content", "ParentId", "Time", "UserId" },
                 values: new object[] { 2, "test", 2, "10-10-2020", "35606eb1-8062-420c-8eb2-fe8eaa26c73f" });
@@ -14,7 +14,7 @@ namespace Twitter.Data.Migrations
             migrationBuilder.InsertData(
                 table: "Tweets",
                 columns: new[] { "Id", "Content", "ParentId", "Time", "UserId" },
-                values: new object[] { 1, "test", 2, "10-10-2020", "35606eb1-8062-420c-8eb2-fe8eaa26c73f" });
+                values: new object[] { 1, "test", 2, "10-10-2020", "35606eb1-8062-420c-8eb2-fe8eaa26c73f" });*/
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ Twitter/Twitter/Data/Migrations/20210620114500_DeleteSeeding.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Twitter.Data.Migrations
+{
+    public partial class DeleteSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tweets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tweets",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tweets",
+                columns: new[] { "Id", "Content", "ParentId", "Time", "UserId" },
+                values: new object[] { 2, "test", 2, "10-10-2020", "35606eb1-8062-420c-8eb2-fe8eaa26c73f" });
+
+            migrationBuilder.InsertData(
+                table: "Tweets",
+                columns: new[] { "Id", "Content", "ParentId", "Time", "UserId" },
+                values: new object[] { 1, "test", 2, "10-10-2020", "35606eb1-8062-420c-8eb2-fe8eaa26c73f" });
+        }
+    }
+}
